94b512555b7440a7babda51a8b704309ce174fd1
szilard28 <71643751+szilard28@users.noreply.github.com>
2020-09-21 15:02:54 +0200

    Update and rename README.md to otsog.py

---
+++ otsog.py
@@ -0,0 +1,14 @@
+import turtle              
+wn = turtle.Screen()        
+alex = turtle.Turtle()     
+alex.forward(75)          
+alex.left(72)               
+alex.forward(75)           
+alex.left(72)
+alex.forward(75) 
+alex.left(72)
+alex.forward(75)
+alex.left(72)
+alex.forward(75)
+alex.left(72)
+ 
